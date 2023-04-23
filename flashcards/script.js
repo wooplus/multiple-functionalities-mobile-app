ff3d8d40467ed55e1626a394f992856e8a1d2cff
linnhtetaung <you@example.com>
2023-04-22 23:30:49 -0400

    v-1.8 fixed navbar added database to quizz app

--- flashcards/script.js
+++ flashcards/script.js
@@ -1,55 +1,88 @@
 const answerField = document.getElementById("answer-field");
 const questionField = document.getElementById("question-field");
-import data from "./data.js";
+const flashContainerEl = document.getElementById("flash-container");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
-render(data);
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSetting = {
+  databaseURL: "https://mobileshoppingapp-58bfd-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSetting);
+const database = getDatabase(app);
+const flashquizzInDB = ref(database, "flashQuizz");
 
 document.getElementById("add-quiz-btn").addEventListener("click", (e) => {
+  console.log("click");
   e.preventDefault();
-  data.push({
-    question: answerField.value,
-    answer: questionField.value,
-  });
-  render(data);
-  console.log(answerField.value, questionField.value);
+  push(flashquizzInDB, [questionField.value, answerField.value]);
+  clearInputsData();
+  console.log("added to database");
 });
 
-function render(data) {
-  getQuizzElements(data);
-  flipBackAction();
-  clearInputsData();
-}
+onValue(flashquizzInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+    let itemsArr = Object.entries(snapshot.val());
+    clearQuizzElements();
+    for (let item of itemsArr) {
+      appendQuizzElements(item);
+    }
+    // flipBackAction()
+  } else {
+    flashContainerEl.innerHTML = "No flashcards created yet...";
+  }
+});
 
-function getQuizzElements(data) {
-  let flashText = ``;
-  for (let quizz of data) {
-    flashText += `<div class="flashcard" id="flashcard">
-        <div class="front" id="front">
+function appendQuizzElements(item) {
+  const itemID = item[0];
+  const quizz = item[1];
+  const [question, answer] = quizz;
+  const newQuizz = document.createElement("div");
+  newQuizz.classList.add("flashcard");
+  newQuizz.innerHTML = ` <div class="front" id="front">
           <h2>Question</h2>
-          <p>${quizz.question}</p>
+          <p>${question}</p>
         </div>
         <div class="back" id="back">
           <h2>Answer</h2>
-          <p>${quizz.answer}</p>
-        </div>
-      </div>`;
-  }
-  document.getElementById("flash-container").innerHTML = flashText;
+          <p>${answer}</p>
+        </div>`;
+  flashContainerEl.appendChild(newQuizz);
+  newQuizz.addEventListener("dblclick", () => {
+    let exactLocationOfItemInDB = ref(database, `flashQuizz/${itemID}`);
+    remove(exactLocationOfItemInDB);
+  });
+  newQuizz.addEventListener("click", (e) => {
+    newQuizz.children[0].classList.toggle("d-none");
+    newQuizz.children[1].classList.toggle("d-block");
+  });
 }
 
-function flipBackAction() {
-  let quizzes = document.getElementsByClassName("flashcard");
-  for (let quizz of quizzes) {
-    console.log(quizz.children);
-    quizz.addEventListener("click", (e) => {
-      console.log("click");
-      quizz.children[0].classList.toggle("d-none");
-      quizz.children[1].classList.toggle("d-block");
-    });
-  }
-}
+// function flipBackAction() {
+//   let quizzes = document.getElementsByClassName("flashcard");
+//   for (let quizz of quizzes) {
+//     console.log(quizz.children);
+//     quizz.addEventListener("click", (e) => {
+//       console.log("click");
+//       quizz.children[0].classList.toggle("d-none");
+//       quizz.children[1].classList.toggle("d-block");
+//     });
+//   }
+// }
 
 function clearInputsData() {
   answerField.value = "";
   questionField.value = "";
+}
+
+function clearQuizzElements() {
+  flashContainerEl.innerHTML = ``;
 }
